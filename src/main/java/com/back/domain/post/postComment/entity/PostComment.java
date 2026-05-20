28698e6109fdbbae932e634df60ec56d1aae5b43
hiysk0610-hi <hiysk0610@gmail.com>
2026-05-20 12:27:59 +0900

    010 댓글 다건조회, 단건조회기능 구현

--- src/main/java/com/back/domain/post/postComment/entity/PostComment.java
+++ src/main/java/com/back/domain/post/postComment/entity/PostComment.java
@@ -1,18 +1,13 @@
-package com.back.domain.post.post.postComment;
+package com.back.domain.post.postComment.entity;
 
 import com.back.domain.post.post.entity.Post;
 import com.back.global.jpa.entity.BaseEntity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity
-@Getter
 @NoArgsConstructor
 public class PostComment extends BaseEntity {
-
     @ManyToOne
     @JsonIgnore
     private Post post;
@@ -23,7 +18,8 @@ public class PostComment extends BaseEntity {
         this.content = content;
     }
 
-    public void modify(String body) {
+    public void modify(String content) {
         this.content = content;
+
     }
 }
