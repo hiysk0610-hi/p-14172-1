b2deaa8252bf4ec0a3ad7a3a0a850b46c298fb43
hiysk0610-hi <hiysk0610@gmail.com>
2026-05-21 00:08:04 +0900

    017 RsData 에서 data 필드를 비우고 싶다면 명시적으로 RsData＜Void＞

--- src/main/java/com/back/global/rsData/RsData.java
+++ src/main/java/com/back/global/rsData/RsData.java
@@ -3,4 +3,8 @@ package com.back.global.rsData;
 import com.back.domain.post.postComment.dto.PostCommentDto;
 
 public record RsData<T>(String resultCode, String msg, T data) {
+        public RsData(String resultCode, String formatted) {
+            this(resultCode, formatted, null);
+        }
 }
+
